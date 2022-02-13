5e3d28bf57f8ca6d3049db592185337c7ed7e5b5
Loy-Yun <yoonsoji@gmail.com>
2022-02-13 15:45:00 +0900

    init:create server app

---
+++ src/main/java/com/absg/catspatchserver/CatspatchServerApplication.java
@@ -0,0 +1,13 @@
+package com.absg.catspatchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatspatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatspatchServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/absg/catspatchserver/CatspatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absg.catspatchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatspatchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
