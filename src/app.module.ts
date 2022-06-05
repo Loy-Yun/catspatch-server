ff16ea0634d5c083ae732c64a6c01ff479d817db
Loy-Yun <yoonsoji@gmail.com>
2022-06-05 16:19:40 +0900

    chore: module - create memo, user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MemoModule } from './memo/memo.module';
+import { MomoController } from './momo/momo.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule, MemoModule],
+  controllers: [AppController, MomoController],
   providers: [AppService],
 })
 export class AppModule {}
